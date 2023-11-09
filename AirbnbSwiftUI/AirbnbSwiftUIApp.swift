05186cf8f54a1a37b7e19dfdd4a76e7b8939ea91
boardguy1024 <boardguy1024@gmail.com>
2023-11-09 21:51:01 +0900

    Initial Commit

---
+++ AirbnbSwiftUI/AirbnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUIApp.swift
+//  AirbnbSwiftUI
+//
+//  Created by park kyung seok on 2023/11/09.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
